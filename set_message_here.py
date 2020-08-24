35ad4759eec2677b00b81bda7199c58463c66d1a
LakshmiNarayana-9990 <56222494+LakshmiNarayana-9990@users.noreply.github.com>
2020-08-24 11:18:21 +0530

    Update set_message_here.py

--- set_message_here.py
+++ set_message_here.py
@@ -1,7 +1,7 @@
 from twilio.rest import Client
 import os
-account_sid = 'AC535a650a257021988975c8a86cb4da55'
-auth_token = '05654fb9f5538ef54a2eacfa707c0ddc'
+account_sid = 'your a/c sid'
+auth_token = 'your auth token'
 client = Client(account_sid, auth_token)
 def msg():
-    client.messages.create(body='Hello!',from_='whatsapp:+14155238886',to='whatsapp:+917396237303')
+    client.messages.create(body='Hello!',from_='whatsapp:+14155238886',to='whatsapp:+91xxxx')
